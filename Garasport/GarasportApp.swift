02768c0e75f06c6a832f3a3f4be8f60e8490f3cc
iosImproveArt <impiosart@gmail.com>
2024-12-26 11:24:58 +0200

    added yaml

---
+++ Garasport/GarasportApp.swift
@@ -0,0 +1,17 @@
+//
+//  GarasportApp.swift
+//  Garasport
+//
+//  Created by Improve on 26.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct GarasportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
